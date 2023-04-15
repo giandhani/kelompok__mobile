9249926cfe0b6362656ff2534396c3bbefc9a656
hilmiihsan29 <130488348+hilmiihsan29@users.noreply.github.com>
2023-04-15 11:40:09 +0700

    Update data_news.dart

--- lib/data/data_news.dart
+++ lib/data/data_news.dart
@@ -1,6 +1,22 @@
 import '../models/news.dart';
 
 List<News> newslist = [
-  News(id: 12345, deskripsi: ".", gambar: ".", judul: "."),
-  News(id: 12435, deskripsi: ".", gambar: ".", judul: ".")
+  News(id: 12345, deskripsi: "Berbicara soal kemunculan Arcturus pada konferensi pers pada 29 Maret, Dr Maria Van Kerkhove, pimpinan teknis WHO untuk Covid, mengatakan bahwa subvarian itu sudah beredar selama beberapa bulan.
+
+"Subvarian ini memiliki satu mutasi tambahan pada lonjakan protein, yang, menurut penelitian laboratorium, menunjukkan peningkatan infektivitas serta potensi peningkatan patogenisitas," kata Van Kerkhove.
+
+Arcturus merupakan salah satu dari 600 subvarian yang dihasilkan dari Omicron sejauh ini, meski tampaknya tidak lebih mematikan dari yang lain. Sejauh ini subvarian tersebut telah terdeteksi di 22 negara, termasuk Inggris dan AS.
+
+Di India, kementerian kesehatan negara itu melaporkan 40.215 kasus Covid aktif pada 12 April, naik 3.122 hanya dalam satu hari. Lonjakan kasus ini mendorong masyarakat untuk kembali wajib mengenakan masker di beberapa negara bagian dan meningkatkan produksi vaksin.
+
+Dr Vipin Vashishtha, seorang dokter anak dan mantan kepala Komite Imunisasi Akademi Pediatri India, mengatakan kepada The Hindustan Times bahwa gejala Arcturus termasuk demam tinggi, batuk dan konjungtivitis atau mata merah yang gatal.
+
+Sekitar 50 kasus telah terdeteksi di Inggris sejauh ini, menurut The Daily Mail, tetapi Profesor Paul Hunter dari University of East Anglia mengatakan kepada bahwa masih terlalu dini untuk mengatakan bahwa Inggris menghadapi lonjakan baru infeksi yang didorong oleh Arcturus.
+
+Sementara itu, para ilmuwan di Universitas Tokyo menyebut bahwa Arcturus menyebar sekitar 1,17 hingga 1,27 kali lebih efisien daripada varian sebelumnya.
+
+Peneliti pun memperingatkan bahwa Arcturus akan menyebar ke seluruh dunia dalam waktu dekat, mengingat bahwa subvarian ini memiliki kekebalan terhadap antibodi yang tertinggal di tubuh dari infeksi Covid sebelumnya.", gambar: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fpmb.brin.go.id%2Fwp-content%2Fuploads%2F2020%2F11%2Fcovid-cells.jpg&tbnid=uKWruViARYQPGM&vet=12ahUKEwiKrvfw_Kr-AhU4_XMBHc-kDswQMygLegUIARCcAQ..i&imgrefurl=https%3A%2F%2Fpmb.brin.go.id%2Fanalisis-lipi-tentang-kebijakan-covid-19-antar-negara-asean%2F&docid=7paUQ7IFL_HmIM&w=2357&h=1326&q=covid&ved=2ahUKEwiKrvfw_Kr-AhU4_XMBHc-kDswQMygLegUIARCcAQ", judul: " Mengenal Covid Arcturus, Subvarian Baru yang Sangat Menular "),
+  News(id: 12435, deskripsi: "lagi-lagi cuma menjadi penghangat bangku cadangan saat KMSK Deinze memainkan laga play-off Liga divisi 2 Belgia 2022-2023. Sialnya, tanpa bantuan Marselino, KMSK Deinze dihajar habis oleh lawannya, yakni Lommel, pada Sabtu (15/4/2023) dini hari WIB.
+
+Pertandingan Lommel vs KMSK Deinze dilangsungkan di Soevereinstadion. Bermain di kandang lawan membuat KMSKI Deinze terlihat kesulitan keluar dari tekanan, alhasil tim tersebut harus menyerah dengan skor yang cukup telak, yakni 1-4.", gambar: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdns.klimg.com%2Fbola.net%2Flibrary%2Fupload%2F21%2F2022%2F11%2F645x430%2Fmarselino-ferdinan-i_1c242b0.jpg&tbnid=bErRVKIb14PJhM&vet=12ahUKEwjsp8iV_qr-AhUYtmMGHWhvA48QMygEegUIARC_AQ..i&imgrefurl=https%3A%2F%2Fwww.bola.net%2Fpiala_dunia%2Fno-marselino-ferdinan-no-problem-timnas-indonesia-u-20-bisa-andalkan-3-pemain-ini-di-piala-as-22c023.html&docid=omgQJAsmUgfqbM&w=645&h=430&q=marselino&ved=2ahUKEwjsp8iV_qr-AhUYtmMGHWhvA48QMygEegUIARC_AQ", judul: "Tak Mainkan Marselino Ferdinan saat Lawan Lommel, KMSK Deinze Tumbang 1-4!")
 ];
